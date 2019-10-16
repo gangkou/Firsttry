410f63b94931d909c4eede8d7e9aed64466cf316
港口 <1593554685@qq.comm>
2019-10-16 14:18:46 +0800

    将重复功能的包删除

--- MyGame/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module MyGame {
-	requires java.desktop;
-}
--- MyGame/src/test/Constant.java
+++
@@ -1,6 +0,0 @@
-package test;
-
-public class Constant {
-	public static final int gamewidth =2000;
-	public static final int gameheight =2000;
-}
